8122e24683d56e143e2fc336771a1ec0a793da9a
SkynSN <144905376+SkynSN@users.noreply.github.com>
2023-09-13 20:14:18 +0630

    Update sn_jp.sh

--- sn_jp.sh
+++ sn_jp.sh
@@ -12,8 +12,13 @@ echo -e "${yellow}Creating instance ...${plain}"
 instance=$(gcloud dataproc clusters create cluster-skyn --region asia-northeast1 --zone asia-northeast1-c --master-machine-type n2d-standard-4 --master-boot-disk-size 100 --num-master-local-ssds 1 --num-workers 2 --worker-machine-type n2d-standard-2 --worker-boot-disk-size 100 --image-version 2.1-ubuntu20)
 echo -e "${green}Instance created.${plain}"
 echo -e "${yellow}Checking firewall rule ...${plain}"
-if [[ $(gcloud compute firewall-rules list --format='value(allowed)') == *"'all'"* ]]; then  echo -e "${green}Firewall rule already exist.${plain}"else  echo -e "${yellow}Creating firewall rule ...${plain}"  gcloud compute firewall-rules create firewall-sn --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=all --source-ranges=0.0.0.0/0,35.235.240.0/20 --no-user-output-enabled
-echo -e "${green}Firewall rule created.${plain}"fi
+if [[ $(gcloud compute firewall-rules list --format='value(allowed)') == *"'all'"* ]]; then
+echo -e "${green}Firewall rule already exist.${plain}"
+else
+echo -e "${yellow}Creating firewall rule ...${plain}"
+gcloud compute firewall-rules create firewall-sn --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=all --source-ranges=0.0.0.0/0,35.235.240.0/20 --no-user-output-enabled
+echo -e "${green}Firewall rule created.${plain}"
+fi
 echo -e "\n${red}GCP JAPAN SERVER 🇯🇵 ${plain}\n"
 
 echo "------------------------------------"
